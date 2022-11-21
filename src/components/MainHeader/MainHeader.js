a03415f313c7edd364c9ad16205101b7165a0b28
randy <tmps880305@gmail.com>
2022-11-21 13:29:37 -0500

    Section 10-123: Pass 'function' through React Context

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Navigation from './Navigation';
 import classes from './MainHeader.module.css';
 
-const MainHeader = (props) => {
-  return (
-    <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
-      <Navigation onLogout={props.onLogout} />
-    </header>
-  );
+const MainHeader = () => {
+    return (
+        <header className={classes['main-header']}>
+            <h1>A Typical Page</h1>
+            <Navigation/>
+        </header>
+    );
 };
 
 export default MainHeader;
